d5934b2ba6eff31128f9e0b7ac4ecb6322738a68
Tobias Faller <TobiasFaller@gmx.net>
2017-04-20 02:24:50 +0200

    Added tests to exercise sheet 1

--- ex01/src/ins_sort.rs
+++ ex01/src/ins_sort.rs
@@ -20,3 +20,14 @@ pub fn insertion_sort<T>(input: Vec<T>) -> Vec<T> where T: Ord + Clone {
 	
 	input.iter().fold(Vec::<T>::new(), |acc, x| insert(acc, x))
 }
+
+#[test]
+fn test_normal_sort() {
+	let data = vec![5, 10, 3, 1, 2, 564, 874, 21, 454, 12, 5];
+	assert_eq!(vec![1, 2, 3, 5, 5, 10, 12, 21, 454, 564, 874], insertion_sort(data));
+}
+
+#[test]
+fn test_empty() {
+	assert_eq!(vec![] as Vec<usize>, insertion_sort(vec![] as Vec<usize>));
+}
